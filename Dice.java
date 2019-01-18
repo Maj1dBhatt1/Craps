a0f6962b7944d1e7e0be4303a6ca2f624861e137
Majid Bhatti <mbhatti19@students.polytechnic.org>
2019-01-18 13:27:05 -0800

    Added Dice class.

---
+++ Dice.java
@@ -0,0 +1,29 @@
+/**
+ * why? ... for the sake of practice.
+ *
+ * @author MajidBhatti
+ * @version 2019-01-18
+ */
+
+ public class Dice
+ {
+   //private int numOfDice
+   private Die d1;
+   private Die d2;
+
+   //constructor
+   public Dice()
+   {
+     d1 = new Die();
+     d2 = new Die();
+   }
+
+   public int[] roll()
+   {
+     int[] rolls = new int[2];
+     rolls[0] = d1.roll();
+     rolls[2] = d2.roll();
+     return rolls;
+   }
+
+ }
